57048f33c4978f9f726a1ccf37a13ebb94704e10
jaehunju1996 <jjdong9108@gmail.com>
2023-02-07 00:10:18 +0900

    To the main branch commit express official document which is includes 'Hello World' example sentence

---
+++ app.js
@@ -0,0 +1,25 @@
+// express 서버의 가장 루트(최상단) 폴더, 중심이 됨. 이 파일 없으면 에러남
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req,res)=>{
+    res.send('GET')
+});
+
+app.post('/', (req,res)=>{
+    res.send('POST')
+})
+
+app.put('/', (req,res)=>{
+    res.send('PUT')
+})
+
+app.delete('/', (req,res)=>{
+    res.send('DELETE')
+})
+
+app.listen(port, ()=>{
+    console.log('Example app listening on port ${port}')
+});
