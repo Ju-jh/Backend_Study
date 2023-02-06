023de6ee21e91a2739a6d4a1f6689f927a1b6af0
jaehunju1996 <jjdong9108@gmail.com>
2023-02-07 00:55:11 +0900

    feat: extra things commmit

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+const connect = require("./schemas");
+connect();
+
 app.get('/', (req,res)=>{
     res.send('GET')
 });
@@ -21,5 +25,5 @@ app.delete('/', (req,res)=>{
 })
 
 app.listen(port, ()=>{
-    console.log('Example app listening on port ${port}')
+    console.log('Example app listening on port', port)
 });
