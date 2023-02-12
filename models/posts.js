bb2e4e645f344865aa5e6c60fc98c8212b5f92d0
jaehunju1996 <jjdong9108@gmail.com>
2023-02-12 18:04:44 +0900

    feat: sequalize-init

---
+++ models/posts.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Posts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Posts.init({
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Posts',
+  });
+  return Posts;
+};
